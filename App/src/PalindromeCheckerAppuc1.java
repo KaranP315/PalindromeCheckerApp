54a7f9720aab5ab4a879795c5686738371c1f05c
unknown <karan2006.p@gmail.com>
2026-02-27 15:29:20 +0530

    [Karan P] Added [UC1]

--- App/src/PalindromeCheckerAppuc1.java
+++ App/src/PalindromeCheckerAppuc1.java
@@ -1,4 +1,4 @@
-public class PalindromeCheckerApp {
+public class PalindromeCheckerAppuc1 {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App Management System ");
         System.out.println("Version 1.0");
