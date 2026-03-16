62bce2a6a62ad29ee639221592aa03bd18481792
KaranP315 <karan2006.p@gmail.com>
2026-03-16 21:45:19 +0530

    [Karan P] Fixed UC9 class name

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UseCase9PalindromeCheckerApp {
+public class git push origin PalindromeCheckerApp {
 
     // Recursive method to check palindrome
     public static boolean isPalindrome(String str, int start, int end) {
