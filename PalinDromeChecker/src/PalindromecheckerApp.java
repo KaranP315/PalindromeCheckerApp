4976120af9d6b379b1434b6508a16c13c40777b2
KaranP315 <karan2006.p@gmail.com>
2026-03-16 21:34:47 +0530

    Update PalindromecheckerApp.java

--- PalinDromeChecker/src/PalindromecheckerApp.java
+++ PalinDromeChecker/src/PalindromecheckerApp.java
@@ -2,37 +2,38 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+public class UseCase7PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        // Insert characters into deque
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
         }
+
         scanner.close();
-    }
-
-    public static boolean isPalindrome(String text) {
-        String cleanText = text.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char ch : cleanText.toCharArray()) {
-            deque.addLast(ch);
-        }
-
-        while (deque.size() > 1) {
-            Character front = deque.removeFirst();
-            Character back = deque.removeLast();
-
-            if (!front.equals(back)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
